2458074cd355feaacbc7467224ec3ef3038625a6
F <foad.alshabibi@gmail.com>
2025-06-25 14:30:52 +0300

    starter

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace RichTextEditorDemo.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
